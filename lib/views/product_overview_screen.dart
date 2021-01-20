d73413a95faf045568e78efe0c994b10063306a1
EDSON MATEUS ALVES DE OLIVEIRA <tioed2015@gmail.com>
2021-01-20 02:46:56 -0300

    filtor

--- lib/views/product_overview_screen.dart
+++ lib/views/product_overview_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gereaciando_estado/models/product.dart';
-import 'package:gereaciando_estado/providers/product_provider.dart';
 import 'package:gereaciando_estado/widgets/product_grid.dart';
-import 'package:provider/provider.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gereaciando_estado/models/product.dart';
-import 'package:gereaciando_estado/providers/product_provider.dart';
 import 'package:gereaciando_estado/utils/app_routes.dart';
-import 'package:gereaciando_estado/views/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
@@ -10,7 +8,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(
       context,
-      listen: true,
+      listen: false,
     ); // listen: pode ser usando para que o provider nao notifique a aplicacao de uma mudanca
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -29,11 +27,15 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            color: Theme.of(context).accentColor,
-            onPressed: product.tooggleFavorite,
+          leading: Consumer<Product>(
+            // e usado para consumir o provider, apenas quem esta dentro do consumir vai sofrer uma renderizacao
+            //child: Text("elemto que nunca muda"),
+            builder: (context, product, child) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).accentColor,
+              onPressed: product.tooggleFavorite,
+            ),
           ),
           title: Text(
             product.title,
